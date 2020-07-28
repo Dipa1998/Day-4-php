838f139bd9c896f92857af7bd334103be343a57c
Dipa1998 <68769603+Dipa1998@users.noreply.github.com>
2020-07-28 20:24:15 +0530

    Add files via upload

---
+++ xml 4.php
@@ -0,0 +1,33 @@
+<Producera>
+	<Producer>
+		<name>Alex</name>
+		<age>21</age>
+		<show>
+			<showname>A show</showname>
+			<showdate>12 th december 2010 </showdate>
+		</show>
+		<show>
+			<showname>Another show</showname>
+			<showdate>12 th july 2009 </showdate>
+		</show>
+	</Producer>
+	<Producer>
+	    <name>billy</name>
+	    <age>16</age>
+	    <show>
+			<showname>Better show</showname>
+			<showdate>12 th january 2010 </showdate>
+		</show>
+</Producer>
+<Producer>
+	    <name>deal</name>
+	    <age>45</age>
+</Producer>
+<Producer>
+	    <name>deal</name>
+	    <age>45</age>
+</Producer>
+</Producer>
+
+
+
